1237af27413a527285244aab1e16cc51047c23a9
kevinorta <kevino6532@hstat.org>
2016-11-06 10:13:52 +0000

    done

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    attendees.collect { |name| 
+        "Hello, my name is #{name}."
+    }
+end
+
+def assign_rooms(attendees)
+    room=0
+    attendees.collect{ |name|
+     room+=1
+        "Hello, #{name}! You'll be assigned to room #{room}!"
+    }
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each { |badge|
+        puts badge
+    }
+    assign_rooms(attendees).each { |roomAssign|
+    puts roomAssign
+    }
+end
